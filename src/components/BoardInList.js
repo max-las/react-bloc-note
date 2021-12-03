cfb7b07aca2949f0a50f7c533a28a118c6a3d673
max-las <lasserre.maxime@yahoo.fr>
2021-12-03 12:25:49 +0100

    update aspect

--- src/components/BoardInList.js
+++ src/components/BoardInList.js
@@ -2,7 +2,7 @@ function BoardInList({board, click}) {
   return (
     <div className="box InSortableList BoardInList" onClick={click}>
       <div className="block">
-        <strong style={{fontSize: "30px"}}>{board.name}</strong>
+        <strong style={{fontSize: "25px"}}>{board.name}</strong>
       </div>
       <div className="block content is-small">
         <p className="has-text-white-ter">Contient {board.note_ids.length} notes</p>
